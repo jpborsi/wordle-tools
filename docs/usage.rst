87cb2b1d3cf5e8f21ce76c5b7e243adcdf77f917
John Borsi <jborsi@proformex.com>
2022-01-21 18:23:29 -0500

    Initial cookiecutter setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use Wordle Tools in a project::
+
+    import wordle_tools
---
+++ wordle_tools/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Wordle Tools."""
+
+__author__ = """John Borsi"""
+__email__ = 'jpborsi@gmail.com'
+__version__ = '0.1.0'
